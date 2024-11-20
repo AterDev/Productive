e72dcddac3a5462f3dda3561ae72bc32b670f32e
chris.chen <chris.chen@didatravel.com>
2024-11-20 18:49:21 +0800

    完善程序处理

--- MultiLanguage/FileContentHelper.cs
+++ MultiLanguage/FileContentHelper.cs
@@ -16,17 +16,21 @@ internal class FileContentHelper
     /// <param name="quate"></param>
     /// <returns></returns>
     /// <exception cref="ArgumentOutOfRangeException"></exception>
-    public static List<string> GetCNContent(string path, string[] patterns, QuoteType quate = QuoteType.Double)
+    public static List<string> GetCNContent(string path, string[] patterns, QuoteType quate = QuoteType.Double, string[]? excludeFiles = null)
     {
         var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories)
             .Where(file => patterns.Contains(Path.GetExtension(file)))
             .ToList();
 
+        if (excludeFiles != null)
+        {
+            files = files.Where(file => !excludeFiles.Contains(Path.GetFileName(file))).ToList();
+        }
         var pattern = quate switch
         {
-            QuoteType.Single => @"'[\u4e00-\u9fa5]+?'",
-            QuoteType.Double => @"""[\u4e00-\u9fa5]+?""",
-            QuoteType.Both => @"['""][\u4e00-\u9fa5]+?['""]",
+            QuoteType.Single => @"'[^'\u4e00-\u9fa5]*[\u4e00-\u9fa5]+[^'\u4e00-\u9fa5]*'",
+            QuoteType.Double => @"""[^""\u4e00-\u9fa5]*[\u4e00-\u9fa5]+[^""\u4e00-\u9fa5]*""",
+            QuoteType.Both => @"(['""])([^\r\n'""]*[\u4e00-\u9fa5][^\r\n'""]*)\1",
             _ => throw new ArgumentOutOfRangeException(nameof(quate), quate, null)
         };
         var regex = new Regex(pattern, RegexOptions.Compiled);
@@ -38,15 +42,13 @@ internal class FileContentHelper
             var matches = regex.Matches(content);
             foreach (Match match in matches)
             {
-                contentSet.Add(match.Value);
+                var value = match.Value.Trim('\'', '"');
+                contentSet.Add(value);
             }
         }
         return contentSet.ToList();
     }
 
-
-    [RequiresUnreferencedCode("Calls System.Text.Json.JsonSerializer.Deserialize<TValue>(String, JsonSerializerOptions)")]
-    [RequiresDynamicCode("Calls System.Text.Json.JsonSerializer.Deserialize<TValue>(String, JsonSerializerOptions)")]
     public static Dictionary<string, string> ReadJsonToDictionary(string path)
     {
         if (File.Exists(path))
@@ -65,7 +67,7 @@ internal class FileContentHelper
     /// <param name="data"></param>
     /// <param name="matchTemplates"></param>
     /// <param name="replaceTemplate"></param>
-    public static void ReplaceContent(string path, string[] patterns, Dictionary<string, string> data, string[] matchTemplates, string[] replaceTemplates)
+    public static void ReplaceContent(string path, string[] patterns, Dictionary<string, string> data, string[] matchTemplates, string[] replaceTemplates, string[]? excludeFiles = null)
     {
         if (matchTemplates.Length != replaceTemplates.Length)
         {
@@ -75,6 +77,10 @@ internal class FileContentHelper
         var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories)
             .Where(file => patterns.Contains(Path.GetExtension(file)))
             .ToList();
+        if (excludeFiles != null)
+        {
+            files = files.Where(file => !excludeFiles.Contains(Path.GetFileName(file))).ToList();
+        }
         foreach (var file in files)
         {
             var content = File.ReadAllText(file);
@@ -82,8 +88,8 @@ internal class FileContentHelper
             {
                 for (var i = 0; i < matchTemplates.Length; i++)
                 {
-                    var match = string.Format(matchTemplates[i], item.Key);
-                    var replace = string.Format(replaceTemplates[i], item.Value);
+                    var match = string.Format(matchTemplates[i], item.Value);
+                    var replace = string.Format(replaceTemplates[i], item.Key);
                     content = content.Replace(match, replace);
                 }
             }
--- MultiLanguage/Program.cs
+++ MultiLanguage/Program.cs
@@ -1,28 +1,43 @@
 ﻿using MultiLanguage;
 
-var path = @"";
+// 前端内容
+var path = @"D:\work\BingoV2\src\Frontend\bingov2\src";
 var patterns = new string[] { ".vue", ".ts" };
-var lines = FileContentHelper.GetCNContent(path, patterns, QuoteType.Both);
+var excludeFiles = new string[] { "enumToString.ts" };
+// var lines = FileContentHelper.GetCNContent(path, patterns, QuoteType.Both, excludeFiles);
 
-//输出
-foreach (var item in lines)
-{
-    Console.WriteLine(item);
-}
+// File.WriteAllLines("./front_cn.txt", lines);
 
-var jsonPath = @"";
+// // 后端内容
+
+// path = @"D:\work\BingoV2\src\Bingo.Service.Open";
+// patterns = new string[] { ".cs" };
+// lines = FileContentHelper.GetCNContent(path, patterns, QuoteType.Both);
+
+// File.WriteAllLines("./back_cn.txt", lines);
+
+
+var jsonPath = @"D:\work\BingoV2\src\Frontend\bingov2\public\locales\zh-CN.json";
 var sourceData = FileContentHelper.ReadJsonToDictionary(jsonPath);
 
 var matchTemplates = new string[]
 {
-    "label:'{0}'",
-    "placeholder:'{0}'"
+    "label=\"{0}\"",
+    "placeholder=\"{0}\"",
+    "v-tooltip=\"'{0}'\"",
+    "Name: '{0}'",
+    "title: '{0}'",
+    "subTitle: '{0}'",
 };
 
 var replaceTemplates = new string[]
 {
-    ":label:'{0}'",
-    ":placeholder:'{0}'"
+    ":label=\"$t('{0}')\"",
+    ":placeholder=\"$t('{0}')\"",
+    "v-tooltip=\"$t('{0}')\"",
+    "Name: this.trans('{0}')",
+    "title: this.trnas('{0}')",
+    "subTitle: this.trans('{0}')",
 };
 
-FileContentHelper.ReplaceContent(path, patterns, sourceData, matchTemplates, replaceTemplates);
+FileContentHelper.ReplaceContent(path, patterns, sourceData, matchTemplates, replaceTemplates, excludeFiles);
